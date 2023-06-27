a5aeb08d8dd4c01ced54f27ae6f8e37342cdb0a4
trungnguyenngoc123 <137521550+trungnguyenngoc123@users.noreply.github.com>
2023-06-27 13:31:51 +0700

    chỉnh sửa lần 2

--- mae1.cpp
+++ mae1.cpp
@@ -5,6 +5,7 @@ void inmatran(int a[][100], int m, int n);
 void congmatran(int a[][100], int b[][100], int c[][100], int m, int n);
 void hieumatran(int a[][100], int b[][100], int c[][100], int m, int n);
 void tichmatran(int a[][100], int b[][100], int c[][100], int m, int n, int p);
+int menu ();
 
 //Ham nhapmatran dung de nhap vao ma tran theo tung vi tri khac nhau
 void nhapmatran(int a[][100], int m, int n) {
@@ -57,40 +58,78 @@ void tichmatran(int a[][100], int b[][100], int c[][100], int m, int n, int p) {
 }
 
 int main() {
-    int a[100][100], b[100][100], c[100][100];
-    int m, n, p;
-	//Nhap vao so hang va cot cua ma tran A va B (so cot cua ma tran A = so hang ma tran B)
-    printf("Nhap so hang cua ma tran A: ");
-    scanf("%d", &m);
-    printf("Nhap so cot cua ma tran A ung voi so hang cua ma tran B: ");
-    scanf("%d", &n);
-    printf("Nhap so cot cua ma tran B: ");
-    scanf("%d", &p);
-	
-	//Nhap vao cac phan tu cua ma tran
-    printf("\nNhap ma tran A:\n");
-    nhapmatran(a, m, n);
-
-    printf("\nNhap ma tran B:\n");
-    nhapmatran(b, n, p);
-	
-	//In ra ma tran da nhap
-    printf("\nMa tran A:\n");
-    inmatran(a, m, n);
-
-    printf("\nMa tran B:\n");
-    inmatran(b, n, p);
-	
-	//in ra ket qua phep tinh ma tran
-    congmatran(a, b, c, m, n);
-    printf("\nKet qua ma tran A + B:\n");
-    inmatran(c, m, n);
-
-    hieumatran(a, b, c, m, n);
-    printf("\nKet qua ma tran A - B:\n");
-    inmatran(c, m, n);
-
-    tichmatran(a, b, c, m, n, p);
-    printf("\nKet qua ma tran A * B:\n");
-    inmatran(c, m, p);
+	int luachon;
+    do {
+        nhaplai:
+        luachon = menu();
+        switch(luachon) {
+            case 1:
+            	int a[100][100], b[100][100], c[100][100];
+			    int m, n, p;
+				//Nhap vao so hang va cot cua ma tran A va B (so cot cua ma tran A = so hang ma tran B)
+			    printf("Nhap so hang cua ma tran A: ");
+			    scanf("%d", &m);
+			    printf("Nhap so cot cua ma tran A ung voi so hang cua ma tran B: ");
+			    scanf("%d", &n);
+			    printf("Nhap so cot cua ma tran B: ");
+			    scanf("%d", &p);
+            	//Nhap vao cac phan tu cua ma tran gia tri phan tu khong qua 100
+                printf("\nNhap ma tran A:\n");
+                nhapmatran(a, m, n);
+				printf("\nNhap ma tran B:\n");
+				nhapmatran(b, n, p);
+				
+				//In ra ma tran da nhap
+				printf("\nMa tran A:\n");
+    			inmatran(a, m, n);
+    			printf("\nMa tran B:\n");
+    			inmatran(b, n, p);
+                break;
+            case 2:
+                //in ra ket qua phep tinh ma tran
+			    congmatran(a, b, c, m, n);
+			    printf("\nKet qua ma tran A + B:\n");
+			    inmatran(c, m, n);
+			
+			    hieumatran(a, b, c, m, n);
+			    printf("\nKet qua ma tran A - B:\n");
+			    inmatran(c, m, n);
+			
+			    tichmatran(a, b, c, m, n, p);
+			    printf("\nKet qua ma tran A * B:\n");
+			    inmatran(c, m, p);
+                break;
+            case 3:
+            	
+			    break;
+			case 4:
+				
+			    break;
+			case 5:
+			
+                break;
+            case 0:
+                printf ("Ket thuc chuong trinh, tam biet !");
+                break;
+            default:
+                printf ("Vui long chon dung chuc nang, vui long nhap lai: ");
+                goto nhaplai; 
+        }
+    } while (luachon != 0);
+    return 0;
 }
+
+int menu() {
+    int c;
+    printf ("\n==============MENU===========\n");
+    printf ("\n1. Nhap vao ma tran va in ra ma tran");
+    printf ("\n2. Tinh tong, hieu, tich ma tran");
+    printf ("\n3. Tim reduced-echelon matrix sau do giai he phuong trinh");
+	printf ("\n4. Tim ma tran nghich dao");
+	printf ("\n5. Cheo hoa ma tran");
+    printf ("\n0. Ket thuc\n");
+    printf ("Ban chon chuc nang nao: ");
+    scanf ("%d", &c);
+    return c;
+}
+
